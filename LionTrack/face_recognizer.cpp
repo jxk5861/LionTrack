03c5756c9f79e80593a911a8bb3d5b5ade8ad15b
Jacob Karabin <jjkarabin@gmail.com>
2021-04-17 18:58:17 -0400

    Changes by Kevin Wang

--- LionTrack/face_recognizer.cpp
+++ LionTrack/face_recognizer.cpp
@@ -3,6 +3,6 @@
 using namespace cv::face;
 
 Ptr<LBPHFaceRecognizer> recognizer = face::LBPHFaceRecognizer::create();
-recognizer.read(FileNode("trainer/trainer.yml"));//***Going to need a trainer folder and trainer.yml file with it
+recognizer.read(FileNode("../trainer/trainer.yml"));//***Going to need a trainer folder and trainer.yml file with it
 
 CascadeClassifier faceCascade("../opencv/haarcascades/haarcascade_frontalface_default.xml");
