5944b25b1e6043b791c5faa5ec8aab7067e984e3
Jacob Karabin <jjkarabin@gmail.com>
2021-04-17 18:38:19 -0400

    finished training.cpp

--- LionTrack/training.cpp
+++ LionTrack/training.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <sys/time.h>
 #include <string>
 #include <opencv2/core/core.hpp>
@@ -7,6 +8,8 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/face.hpp>
+#include <opencv2/core/mat.hpp>
 #include <fstream>
 
 using namespace std;
@@ -15,9 +18,14 @@ using namespace cv;
 int main() {
     // Now we train a model to recognize the friendly faces stored in the dataset.
 
+    // The location of the stored images.
+    string path = "../dataset";
+
+    // Recognizer
+    Ptr<LBPHFaceRecognizer> recognizer = face::LBPHFaceRecognizer::create();
+
     // Load a the downloaded opencv cascade https://github.com/opencv/opencv/tree/master/data/haarcascades
     CascadeClassifier faceCascade("../opencv/haarcascades/haarcascade_frontalface_default.xml");
-
 
 
     return 0;
