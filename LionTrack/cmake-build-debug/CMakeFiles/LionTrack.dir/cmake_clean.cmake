43585fc7ada8c3fe37c94f24db60845d7a4e1040
Kevin <wangatang53@gmail.com>
2021-04-17 13:52:35 -0400

    Initial LionTrack project commit

---
+++ LionTrack/cmake-build-debug/CMakeFiles/LionTrack.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LionTrack.dir/main.cpp.obj"
+  "LionTrack.exe"
+  "LionTrack.exe.manifest"
+  "LionTrack.lib"
+  "LionTrack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LionTrack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
