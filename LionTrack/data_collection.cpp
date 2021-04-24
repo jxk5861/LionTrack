aa359dc90d4a5071e270589aedae37fb2953e6d1
Jacob Karabin <jjkarabin@gmail.com>
2021-04-24 17:15:11 -0400

    fdfdf

--- LionTrack/data_collection.cpp
+++ LionTrack/data_collection.cpp
@@ -1,32 +1,22 @@
-#include <opencv2/face.hpp>
+#include <iostream>
+#include <vector>
+#include <sys/time.h>
+#include <string>
 #include <opencv2/core/core.hpp>
 #include <opencv2/objdetect.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <map>
-#include <sstream>
-#include <iostream>
 #include <fstream>
-#include <chrono>
-#include <cmath>
 
 using namespace std;
 using namespace cv;
-using namespace face;
-using namespace chrono;
 
-int main(){
-    Ptr<LBPHFaceRecognizer> recognizer = face::LBPHFaceRecognizer::create();
-    ofstream logfile;
-    logfile.open("logfile.txt", ios_base::app);
+int main() {
+    // First we gather the data of a friendly face. This will allow the us to train the recognizer to recognize a face.
 
-    recognizer->read("../trainer/trainer.yml");//***Going to need a trainer folder and trainer.yml file with it
-
+    // Load a the downloaded opencv cascade https://github.com/opencv/opencv/tree/master/data/haarcascades
     CascadeClassifier faceCascade("../opencv/haarcascades/haarcascade_frontalface_default.xml");
-
-    map<int, string> id_to_name = { {0, "Kevin"}, {1, "Jacob"} };
-
     VideoCapture capture = VideoCapture(0);
     capture.set(CAP_PROP_FRAME_WIDTH, 640);
     capture.set(CAP_PROP_FRAME_HEIGHT, 480);
@@ -37,15 +27,23 @@ int main(){
     }
 
     Mat frame, gray;
+    int face_count = 0;
+    auto face_id = "William";// change depending on person being data-mined.
+    int startTime = 0, finishTime = 0;
+    ofstream logfile;
 
     while(true){
+        // Read the cameras current frame.
         capture >> frame;
+
+        startTime = time(0); //getting the start time of the detection process
 
         if(frame.empty()) {
             break;
         }
 
         flip(frame, frame, 1);
+//        imshow("Video", frame);
         cvtColor(frame, gray, COLOR_BGR2GRAY);
 
         vector<Rect> faces;
@@ -57,90 +55,43 @@ int main(){
         // Search for faces in the image.
         faceCascade.detectMultiScale(gray, faces, scaleFactor, minNeighbors, flags, minSize);
 
-        milliseconds startTime = duration_cast< milliseconds >(
-                system_clock::now().time_since_epoch()
-        );
-
-        int face_count_min = min((int)faces.size(), 3);
-
-        for(int i = 0; i < face_count_min; i++) {
-            Rect r = faces[i];
+        // A face has been detected at (x,y) with size (w,h)
+        for(Rect r : faces) {
             int x = r.x;
             int y = r.y;
             int w = r.width;
             int h = r.height;
 
+            face_count++;
+
             Scalar color(255, 0, 0);
             // Draw a rectangle on the image where the face is detected.
             rectangle(frame, r, color);
-
-            // Crop the image to just the detected face.
             Mat detected_face_grayscale = gray.colRange(x, x + w).rowRange(y, y + h);
-
-            int id;
-            double confidence;
-            recognizer->predict(detected_face_grayscale, id, confidence);
-
-            string label;
-            stringstream stream;
-
-            //confidence = 100 - confidence;
-
-            if(confidence < 30){
-                stream << id_to_name[id];
-//                cout << id_to_name[id] << " confidence: " << 100 - confidence << endl;
-            } else {
-                stream << "Unknown";
-//                cout << "Unknown confidence: " << 100 - confidence << endl;
-            }
+            stringstream path;
+            path << "../dataset/Users." << face_id << "." << face_count << ".jpg";
+            imwrite(path.str(), detected_face_grayscale);
+            finishTime = time(0);
 
 
-            stream << " " << id << " "  << round(100 - confidence);
-            label = stream.str();
+            //logging response time to a text file
+            logfile.open("logfile.txt", ios_base::app);
+            logfile << "Response time: " << finishTime - startTime << "ms" << endl;
 
-            cv::putText(frame, label, Point(x+5, y-5), FONT_HERSHEY_PLAIN, 1, color);
+            logfile.close();
         }
-
-        milliseconds finishTime = duration_cast< milliseconds >(
-                system_clock::now().time_since_epoch()
-        );
-        //logging response time to a text file
-
-
-        if(faces.size() > 0)
-        {
-            int totalTime = finishTime.count() - startTime.count();
-            logfile << "Response time: " << totalTime << "ms";
-
-            if(totalTime < 120*face_count_min)
-            {
-                logfile << " Deadline Met" << endl;
-            }
-            else
-            {
-                logfile << " Deadline Missed" << endl;
-            }
-        }
-
-
+        // Show the image window.
         imshow("detected face", frame);
 
         // Press q to exit from window. Also this provides a needed delay between displaying each frame.
-        char c = (char) waitKey(1000 / 60);
+        char c = (char) waitKey(30);
         if( c == 27 || c == 'q' || c == 'Q' ) {
-
-            if(logfile)
-            {
-                logfile << "--------" << endl;
-                logfile.close();
-            }
+            break;
+        }
+        if(face_count > 50){
             break;
         }
     }
 
-    if(logfile)
-    {
-        logfile << "--------" << endl;
-        logfile.close();
-    }
+    return 0;
 }
