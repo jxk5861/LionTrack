2263c006716ccdc7d7fed393be581f189c98bf33
Kevin <wangatang53@gmail.com>
2021-04-24 21:00:35 -0400

    Updating LionTrack

---
+++ LionTrack/CMakeFiles/LionTrack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LionTrack.dir/face_recognizer.cpp.o"
+  "LionTrack"
+  "LionTrack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LionTrack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
